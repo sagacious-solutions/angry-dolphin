da8ccd0f13f0fc2131d4a1154ba1d89cfc273a36
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-03 06:40:10 -0700

    print message on element location

--- task.py
+++ task.py
@@ -19,6 +19,16 @@ def minimal_task():
     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtUserName$txField", USER_NAME + '\n')
     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtPassword$txField", PASSWORD + '\n')
     VIRTUAL_BROWSER.click_element_when_visible("TransactionMainContent_MyAccountsControl_rptMembership_rptAccounts_0_rowAccount_0")
+
+    # append end with number to iterate items
+    DESCRIPTION_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblDescription_"
+
+    for x in range(0, 20):
+        # is_element_attribute_equal_to(locator: str, attribute: str, expected: str) → boo
+        if VIRTUAL_BROWSER.is_element_attribute_equal_to(DESCRIPTION_ELEMENT_ID + x, "value", "Online Transfer Out") :
+            print("HAHA SAME!")
+            print(x)
+    
     # time.sleep(4)
     time.sleep(10);
     print("Done.")
