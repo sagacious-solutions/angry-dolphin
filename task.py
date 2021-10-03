af9cb030ca90299d25716efe7abf2b6aab187469
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-03 09:02:45 -0700

    now able to seperate out numbers from file to find latest

--- task.py
+++ task.py
@@ -1,6 +1,10 @@
 import time
+import re
 
 from RPA.Robocorp.Vault import Vault
+from RPA.Browser.Selenium import Selenium
+from RPA.Excel.Files import Files
+from RPA.FileSystem import FileSystem
 
 credential_secrets = Vault().get_secret("banking")
 USER_NAME = credential_secrets["username"]
@@ -10,30 +14,50 @@ BANKING_LOGIN_URL= credential_secrets["login_url"]
 transaction_secrets = Vault().get_secret("transactions")
 PAYCHEQUE_DEPOSIT_DESCRIPTION = transaction_secrets["payroll_deposit_label"]
 
+budget_spreadsheet_directory = "./DataSets"
 
-from RPA.Browser.Selenium import Selenium
 VIRTUAL_BROWSER = Selenium()
+FILE_SYSTEM_CONTROLLER = FileSystem()
 
 
-def retrieve_pay_amount():
-    VIRTUAL_BROWSER.open_available_browser(BANKING_LOGIN_URL)
-    # VIRTUAL_BROWSER.click_element_if_visible("q")
-    VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtUserName$txField", USER_NAME + '\n')
-    VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtPassword$txField", PASSWORD + '\n')
-    VIRTUAL_BROWSER.click_element_when_visible("TransactionMainContent_MyAccountsControl_rptMembership_rptAccounts_0_rowAccount_0")
+# def retrieve_pay_amount():
+#     VIRTUAL_BROWSER.open_available_browser(BANKING_LOGIN_URL)
+#     # VIRTUAL_BROWSER.click_element_if_visible("q")
+#     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtUserName$txField", USER_NAME + '\n')
+#     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtPassword$txField", PASSWORD + '\n')
+#     VIRTUAL_BROWSER.click_element_when_visible("TransactionMainContent_MyAccountsControl_rptMembership_rptAccounts_0_rowAccount_0")
 
-    DESCRIPTION_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblDescription_"
-    VALUE_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblAmount_"
-    ROW_COUNT = VIRTUAL_BROWSER.get_element_count("class:item")
+#     DESCRIPTION_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblDescription_"
+#     VALUE_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblAmount_"
+#     ROW_COUNT = VIRTUAL_BROWSER.get_element_count("class:item")
 
-    # looks through each individual transaction listed to see if the description matches a pay roll deposit
-    for x in range(0, ROW_COUNT):
-        if VIRTUAL_BROWSER.is_element_attribute_equal_to(DESCRIPTION_ELEMENT_ID + str(x), "innerText", PAYCHEQUE_DEPOSIT_DESCRIPTION) :
-            PAYCHEQUE_AMMOUNT = VIRTUAL_BROWSER.get_element_attribute(VALUE_ELEMENT_ID + str(x), "innerText")
-            print("You have aquired " + str(PAYCHEQUE_AMMOUNT) + "CAD in currency")
+#     # looks through each individual transaction listed to see if the description matches a pay roll deposit
+#     for x in range(0, ROW_COUNT):
+#         if VIRTUAL_BROWSER.is_element_attribute_equal_to(DESCRIPTION_ELEMENT_ID + str(x), "innerText", PAYCHEQUE_DEPOSIT_DESCRIPTION) :
+#             PAYCHEQUE_AMMOUNT = VIRTUAL_BROWSER.get_element_attribute(VALUE_ELEMENT_ID + str(x), "innerText")
+#             print("You have aquired " + str(PAYCHEQUE_AMMOUNT) + "CAD in currency")
     
-    print("Done.")
+#     print("Done.")
 
+def get_latest_spreadsheet(sheets):
+    year = 1997
+    month = 1
+    day = 1
+
+    print("retrieve latest spreadsheet")
+    for sheet in sheets :
+        print(sheet.name)
+        date_array = sheet.name.split('-')
+        year = date_array[0]
+        month = date_array[1]
+        day = date_array[2].split('.')[0]
+        print(year, month, day)
+
+
+def clone_budget_spreadsheet():
+    all_budget_spreadsheets = FILE_SYSTEM_CONTROLLER.list_files_in_directory(budget_spreadsheet_directory)
+    get_latest_spreadsheet(all_budget_spreadsheets)
 
 if __name__ == "__main__":
-    retrieve_pay_amount()
+    # retrieve_pay_amount()
+    clone_budget_spreadsheet()
