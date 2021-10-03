2921a1b82ae2efd7f9ae1de0e511713c3810d107
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-03 07:48:02 -0700

    removed debugging messages

--- task.py
+++ task.py
@@ -16,33 +16,28 @@ from RPA.Browser.Selenium import Selenium
 VIRTUAL_BROWSER = Selenium()
 
 
-def minimal_task():
+def retrieve_pay_amount():
     VIRTUAL_BROWSER.open_available_browser(BANKING_LOGIN_URL)
     # VIRTUAL_BROWSER.click_element_if_visible("q")
     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtUserName$txField", USER_NAME + '\n')
     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtPassword$txField", PASSWORD + '\n')
     VIRTUAL_BROWSER.click_element_when_visible("TransactionMainContent_MyAccountsControl_rptMembership_rptAccounts_0_rowAccount_0")
 
-    # append end with number to iterate items
     DESCRIPTION_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblDescription_"
     VALUE_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblAmount_"
     ROW_COUNT = VIRTUAL_BROWSER.get_element_count("class:item")
 
+    # looks through each individual transaction listed to see if the description matches a pay roll deposit
     for x in range(0, ROW_COUNT):
-        # is_element_attribute_equal_to(locator: str, attribute: str, expected: str) → boo
         if VIRTUAL_BROWSER.is_element_attribute_equal_to(DESCRIPTION_ELEMENT_ID + str(x), "innerText", PAYCHEQUE_DEPOSIT) :
-            print("WEEEE MONEY!!!")
             PAYCHEQUE_AMMOUNT = VIRTUAL_BROWSER.get_element_attribute(VALUE_ELEMENT_ID + str(x), "innerText")
-            print("BRUH! You got paid " + str(PAYCHEQUE_AMMOUNT))
-        else :
-            print("naw dawg, probably bills")
-            print(DESCRIPTION_ELEMENT_ID + str(x))
+            print("You have aquired " + str(PAYCHEQUE_AMMOUNT) + "CAD in currency")
     
     print("Done.")
 
 
 if __name__ == "__main__":
-    minimal_task()
+    retrieve_pay_amount()
 
 
 
