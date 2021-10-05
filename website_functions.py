0be0e6224b6900514f9cab35acb95c821906a6f6
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-05 15:53:39 -0700

    renamed file and added function to fetch power bill

--- website_functions.py
+++ website_functions.py
@@ -2,24 +2,26 @@ from RPA.Browser.Selenium import Selenium
 
 from RPA.Robocorp.Vault import Vault
 
-credential_secrets = Vault().get_secret("banking")
-USER_NAME = credential_secrets["username"]
-PASSWORD = credential_secrets["password"]
-BANKING_LOGIN_URL= credential_secrets["login_url"]
-
-transaction_secrets = Vault().get_secret("transactions")
-PAYCHEQUE_DEPOSIT_DESCRIPTION = transaction_secrets["payroll_deposit_label"]
+import time
 
 VIRTUAL_BROWSER = Selenium()
 
 # This function logs into online banking, Its generalized to reuse later
 def login_to_banking():
+    credential_secrets = Vault().get_secret("banking")
+    USER_NAME = credential_secrets["username"]
+    PASSWORD = credential_secrets["password"]
+    BANKING_LOGIN_URL= credential_secrets["login_url"]
+
     VIRTUAL_BROWSER.open_available_browser(BANKING_LOGIN_URL)
     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtUserName$txField", USER_NAME + '\n')
     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtPassword$txField", PASSWORD + '\n')
 
 # This function retrieves the last paycheque amount
 def retrieve_pay_amount():
+    transaction_secrets = Vault().get_secret("transactions")
+    PAYCHEQUE_DEPOSIT_DESCRIPTION = transaction_secrets["payroll_deposit_label"]
+
     login_to_banking()
 
     VIRTUAL_BROWSER.click_element_when_visible("TransactionMainContent_MyAccountsControl_rptMembership_rptAccounts_0_rowAccount_0")
@@ -36,3 +38,22 @@ def retrieve_pay_amount():
     
     print("Done.")
     return PAYCHEQUE_AMMOUNT
+
+def get_power_bill():
+    bills_secrets = Vault().get_secret("bills")
+    POWER_PROVIDER_LOGIN = bills_secrets["power_provider_login"]
+    LOGIN_EMAIL = bills_secrets["email"]
+    LOGIN_PASSWORD = bills_secrets["password"]
+
+    print ("FETCHING POWER BILL")
+    VIRTUAL_BROWSER.open_available_browser(POWER_PROVIDER_LOGIN)
+    VIRTUAL_BROWSER.input_text_when_element_is_visible("email", LOGIN_EMAIL)
+    VIRTUAL_BROWSER.input_text_when_element_is_visible("password", LOGIN_PASSWORD + '\n')
+    # time.sleep(5)
+    VIRTUAL_BROWSER.wait_until_element_is_visible("class=bill_amount", 10,"Unable to locate amount due")
+    BILL_AMMOUNT_STRING = VIRTUAL_BROWSER.get_element_attribute("class=bill_amount", "innerText")
+
+    print("Your power bill amount is ", BILL_AMMOUNT_STRING)
+
+
+    return BILL_AMMOUNT_STRING.split('$')[1]
