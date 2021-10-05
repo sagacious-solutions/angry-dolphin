873f7b70c80093a44c6ed2057e78581489888dd7
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-05 07:38:56 -0700

    robot now ignores files that aren't isodate format starting with current year

--- file_name_helpers.py
+++ file_name_helpers.py
@@ -1,22 +1,29 @@
 import datetime
 from datetime import timedelta, date
 
+# def parse_filename(filename):
+    
+
 # This gets passed a list of my budget spread sheets, it determines the latest one based on FILENAME formatted as (YEAR-MONTH-DAY) eg.(2021-10-4)
-def get_latest_spreadsheet(sheets):
+def get_latest_filename(all_filenames):
     TEN_YEARS_AGO_IN_WEEKS = 520
-    latest_budget_file_name = datetime.datetime.now() - timedelta(weeks = TEN_YEARS_AGO_IN_WEEKS)
+    latest_budget_file_name = datetime.datetime.now() - timedelta(weeks = TEN_YEARS_AGO_IN_WEEKS) # initialize to be old
 
-    for sheet in sheets :
-        string_date_array = sheet.name.split('.')[0].split('-')
-        date_array = []
+    print ()    
 
-        for date in string_date_array:
-            date_array.append(int(date))
 
-        budget_file_date = datetime.datetime(date_array[0],date_array[1],date_array[2])      
+    for filename in all_filenames :
+        if filename.name.startswith(str(datetime.datetime.now().date().year)):
+            string_date_array = filename.name.split('.')[0].split('-')
+            date_array = []
 
-        if budget_file_date > latest_budget_file_name:
-            latest_budget_file_name = budget_file_date
+            for date in string_date_array:
+                date_array.append(int(date))
+
+            budget_file_date = datetime.datetime(date_array[0],date_array[1],date_array[2])      
+
+            if budget_file_date > latest_budget_file_name:
+                latest_budget_file_name = budget_file_date
 
     return latest_budget_file_name
 
