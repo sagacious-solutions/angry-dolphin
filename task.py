85bcd4a9895c05c02f6aeccdcfcb9cec87a9e709
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-05 08:44:49 -0700

    refactored virtual browser controls into their own file

--- task.py
+++ task.py
@@ -1,8 +1,7 @@
 from file_name_helpers import get_latest_filename, name_next_sheet, make_file_name
-
+from online_banking_functions import retrieve_pay_amount
 
 from RPA.Robocorp.Vault import Vault
-from RPA.Browser.Selenium import Selenium
 
 from RPA.Excel.Files import Files
 from RPA.Tables import Tables
@@ -17,32 +16,19 @@ BANKING_LOGIN_URL= credential_secrets["login_url"]
 transaction_secrets = Vault().get_secret("transactions")
 PAYCHEQUE_DEPOSIT_DESCRIPTION = transaction_secrets["payroll_deposit_label"]
 
-BUDGET_SPREADSHEET_DIRECTORY = "./DataSets"
 
-VIRTUAL_BROWSER = Selenium()
+# Robot Framework Controllers
 FILE_SYSTEM_CONTROLLER = FileSystem()
+
 PAY_PERIOD_LENGTH_IN_DAYS = 14
+
+# Parameters for modifying spread sheet
+BUDGET_SPREADSHEET_DIRECTORY = "./DataSets"
 PRIMARY_WORKSHEET = "monthly"
+BUDGET_COLUMN_HEIGHT = 12 # Current amount of rows for bills
 
 
-# def retrieve_pay_amount():
-#     VIRTUAL_BROWSER.open_available_browser(BANKING_LOGIN_URL)
-#     # VIRTUAL_BROWSER.click_element_if_visible("q")
-#     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtUserName$txField", USER_NAME + '\n')
-#     VIRTUAL_BROWSER.input_text_when_element_is_visible("ctl00$MainContentFull$ebLoginControl$txtPassword$txField", PASSWORD + '\n')
-#     VIRTUAL_BROWSER.click_element_when_visible("TransactionMainContent_MyAccountsControl_rptMembership_rptAccounts_0_rowAccount_0")
 
-#     DESCRIPTION_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblDescription_"
-#     VALUE_ELEMENT_ID = "MainContent_TransactionMainContent_txpTransactions_ctl01_transactionStatementsControl_flwTransactionStatements_GvTransactionStatements_lblAmount_"
-#     ROW_COUNT = VIRTUAL_BROWSER.get_element_count("class:item")
-
-#     # looks through each individual transaction listed to see if the description matches a pay roll deposit
-#     for x in range(0, ROW_COUNT):
-#         if VIRTUAL_BROWSER.is_element_attribute_equal_to(DESCRIPTION_ELEMENT_ID + str(x), "innerText", PAYCHEQUE_DEPOSIT_DESCRIPTION) :
-#             PAYCHEQUE_AMMOUNT = VIRTUAL_BROWSER.get_element_attribute(VALUE_ELEMENT_ID + str(x), "innerText")
-#             print("You have aquired " + str(PAYCHEQUE_AMMOUNT) + "CAD in currency")
-    
-#     print("Done.")
 
 
 # This gets the last pay period workbook and makes a copy of it for the current pay period
@@ -70,14 +56,18 @@ def retrieve_budget_sheet(WORKBOOK_FILENAME):
 # This updates the newly opened sheet with correct bills for the time of the month
 def update_budget_data(sheet, EXCEL_FILE_NAME):
     print("Updating budget spread sheet for", EXCEL_FILE_NAME)
-    PAY_DAT_DAY = int(EXCEL_FILE_NAME.split('-')[2])
 
-    if PAY_DAT_DAY > 15 :
+    PAY_DATE_DAY = int(EXCEL_FILE_NAME.split('-')[2].split('.')[0])
+
+    if PAY_DATE_DAY > 15 :
         print("EARLY PAY")
     else :
         print ("late pay")
 
 
+# This function will copy a column of values from one column to another
+def copy_column(sheet, source, destination):
+    BUDGET_COLUMN_HEIGHT = 12 # Current heig
 
 
 def debugging_deleteLastCreateFile():
